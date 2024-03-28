55814ae766d0e10e6877e0140adc1ea5cb435f44
grace-y <50380991+grace-y@users.noreply.github.com>
2024-03-28 15:14:05 -0500

    Update NumClusters.R
    Add description

--- NumClusters.R
+++ NumClusters.R
@@ -1,4 +1,12 @@
-## Function for evaluating number of clusters
+# FUNCTION: num_clusters
+#
+# Description: calculate number of clusters in a Visium slide. 
+# Cluster is defined as positive spots adjacent to each other.
+#
+# @param coords Coordinates of all positive spots
+# 
+# @return Number of spots in clusters
+
 num_clusters <- function(coords){
   
   d <- pointDistance(coords, lonlat=FALSE)
